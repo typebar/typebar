6bc179c37f9aa46c626b8600af90309760b7d855
y_pe <anton@ilyfa.cc>
2014-03-05 12:58:17 +0000

    Added main project file, typebar.ino

---
+++ src/typebar.ino
@@ -0,0 +1,61 @@
+/* typebar.ino --- 
+ * 
+ * Filename: typebar.ino
+ * Description: Bluetooth Typewriter
+ * Author: Anton Strilchuk, Pedro Kirk
+ * Maintainer: same as above
+ * Created: Wed Mar  5 11:53:29 2014 (+0000)
+ * Version: 0.001
+ * Package-Requires: (bglib, shift_reg)
+ * Last-Updated: Wed Mar  5 12:57:57 2014 (+0000)
+ *           By: anton
+ *     Update #: 1
+ * URL: http://typebar.github.io
+ * Doc URL: http://typebar.github.io
+ * Keywords: Arduino, Typewriter, Bluetooth, Mechanical Keyboard
+ * Compatibility: Arduino 1.0.5++
+ * 
+ */
+
+/* Commentary: 
+ * Turn your beloved old typewriter into a modern computer 
+ * keyboard with a few extra bells and whistles.
+ * 
+ */
+
+/* Change Log:
+ * 
+ * 
+ */
+
+/* Software License:
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ */
+
+/* Hardware License:
+ * Attribution — You must give appropriate credit, provide a link to the license, 
+ * and indicate if changes were made. You may do so in any reasonable manner, 
+ * but not in any way that suggests the licensor endorses you or your use.
+ * NonCommercial — You may not use the material for commercial purposes.
+ * ShareAlike — If you remix, transform, or build upon the material, 
+ * you must distribute your contributions under the same license as the original.
+ */
+
+/* Code: */
+
+
+
+/* typebar.ino ends here */
